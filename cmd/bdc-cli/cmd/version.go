3ff45d9d646396bfe8c641ca1cb2be9c0e5850e8
David Gu <d.gu@sap.com>
2025-04-30 16:19:23 -0700

    fix: use build-time version variable instead of git tags

--- cmd/bdc-cli/cmd/version.go
+++ cmd/bdc-cli/cmd/version.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"os/exec"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+// Version is set during build
+var Version = "dev"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -14,29 +15,10 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of bdc-cli",
 	Long:  `Print the version number of bdc-cli`,
 	Run: func(cmd *cobra.Command, args []string) {
-		version, err := getVersion()
-		if err != nil {
-			fmt.Printf("Error getting version: %v\n", err)
-			return
-		}
-		fmt.Printf("bdc-cli version %s\n", version)
+		fmt.Printf("bdc-cli version %s\n", Version)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
-}
-
-func getVersion() (string, error) {
-	cmd := exec.Command("git", "describe", "--tags", "--abbrev=0")
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-	version := strings.TrimSpace(string(output))
-	// Remove 'v' prefix if present
-	if strings.HasPrefix(version, "v") {
-		version = version[1:]
-	}
-	return version, nil
 }
